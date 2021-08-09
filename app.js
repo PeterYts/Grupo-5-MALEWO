d3101db98fc6a4fa3fe29c12ed3b98667488f7ba
santiago danibele <santiago.danibele@gmail.com>
2021-08-09 14:14:37 -0300

    agregue pagina compras

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3050, ()=>{
+app.listen(3050, () => {
     console.log('Servidor funcionando');
 });
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/register', (req,res)=>{
+app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/compras.html');
 });
