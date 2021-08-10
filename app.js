5defd558c97fd5e81c6fb0e190404851d8b4da14
PeterYts <pedroytsma@gmail.com>
2021-08-09 20:37:16 -0700

    cree la pagina de product detail y termine la pagina de login

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
 app.get('/compras', (req, res) => {
     res.sendFile(__dirname + '/views/compras.html');
+});
+
+app.get('/product', (req, res) => {
+    res.sendFile(__dirname + '/views/product-detail.html');
 });
