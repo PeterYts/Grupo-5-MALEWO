cc311a9e9fb6ee566fd13cb56af605d5419e2008
PeterYts <pedroytsma@gmail.com>
2021-09-30 09:30:57 -0700

    terminada la funcion de registrar usuario

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const {validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const usersFilePath = path.join(__dirname, '../data/Usuarios.json');
 
@@ -12,19 +14,25 @@ const usersController = {
     },
     registrateUser: (req, res) => {
         const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
-		let newUser = req.body
-		newUser['Image'] = req.file.filename;
-		if (users.length === 0) {
-			newUser['id'] = 1;
-			users.push(newUser);
-		}else {
-			let lastUser = users[users.length -1];
-			newUser['id'] = lastUser.id + 1;
-			users.push(newUser);
-		}
-		let changeUser = JSON.stringify(users, null, '  ');
-		fs.writeFileSync(usersFilePath, changeUser);
-		res.redirect('/');
+		let errors = validationResult(req);
+		if (errors.isEmpty()) {
+			let newUser = req.body
+			newUser['Image'] = req.file.filename;
+			if (users.length === 0) {
+				newUser['id'] = 1;
+				users.push(newUser);
+			}else {
+				let lastUser = users[users.length -1];
+				newUser['id'] = lastUser.id + 1;
+				users.push(newUser);
+			}
+			req.body.password = bcrypt.hashSync(req.body.password, 12);
+			let changeUser = JSON.stringify(users, null, '  ');
+			fs.writeFileSync(usersFilePath, changeUser);
+			res.redirect('/');
+		}else 
+			//res.send(errors.mapped());
+			res.render('register', {errors: errors.mapped(), old: req.body});
     }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const path = require('path');
 var router = express.Router();
 const usersController = require("../controllers/usersController");
 const multer = require('multer');
+const { check } = require('express-validator')
 
 const storage = multer.diskStorage({ 
     destination: 'public/img/userImg', 
@@ -12,10 +13,27 @@ const storage = multer.diskStorage({
     }
   });
     
-  const upload = multer({ storage: storage })
+const upload = multer({ storage: storage })
+
+let validationsRegister = [
+  check('name')
+    .notEmpty().withMessage('Este campo es obligatorio').bail()
+    .isLength({max: 16}).withMessage('maximo 16 caracteres'),
+  check('email')
+    .notEmpty().withMessage('Este campo es obligatorio').bail()
+    .isEmail().withMessage('Este campo debe ser un Email'),
+  check('tel')
+    .notEmpty().withMessage('Este campo es obligatorio').bail()
+    .isNumeric().withMessage('Ingrese num de telefono valido').bail()
+    .isLength({min: 14}, {max: 15}),
+  check('password')
+    .notEmpty().withMessage('Este campo es obligatorio').bail()
+    .isLength({max: 16}).withMessage('maximo 16 caracteres').bail()
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, "i")
+];
 
 router.get('/register', usersController.register)
 router.get('/login' , usersController.login );
-router.post('/register/', upload.single('Image'), usersController.registrateUser)
+router.post('/register/', upload.single('Image'), validationsRegister, usersController.registrateUser)
 
 module.exports = router;
