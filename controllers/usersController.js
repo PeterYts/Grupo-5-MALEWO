256aeb4be7d5534717327ed4261a0f3c2643fc61
PeterYts <pedroytsma@gmail.com>
2021-10-07 22:20:27 -0700

    arregle problema con register

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -37,24 +37,28 @@ const usersController = {
     registrateUser: (req, res) => {	
         const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 		let errors = validationResult(req);
-		if (errors.isEmpty()) {
-			let newUser = req.body
-			newUser['Image'] = req.file.filename;
-			if (users.length === 0) {
-				newUser['id'] = 1;
-				users.push(newUser);
-			}else {
-				let lastUser = users[users.length -1];
-				newUser['id'] = lastUser.id + 1;
-				users.push(newUser);
-			}
-			req.body.password = bcrypt.hashSync(req.body.password, 12);
-			let changeUser = JSON.stringify(users, null, '  ');
-			fs.writeFileSync(usersFilePath, changeUser);
-			res.redirect('/');
+		let userToLogin = User.findByField('email', req.body.email);
+		if (userToLogin) {
+			res.render('register', {emailError: 'El email ya esta en uso', old: req.body});
 		}else 
-			//res.send(errors.mapped());
-			res.render('register', {errors: errors.mapped(), old: req.body});
+			if (errors.isEmpty()) {
+				let newUser = req.body
+				newUser['Image'] = req.file.filename;
+				if (users.length === 0) {
+					newUser['id'] = 1;
+					users.push(newUser);
+				}else {
+					let lastUser = users[users.length -1];
+					newUser['id'] = lastUser.id + 1;
+					users.push(newUser);
+				}
+				req.body.password = bcrypt.hashSync(req.body.password, 12);
+				let changeUser = JSON.stringify(users, null, '  ');
+				fs.writeFileSync(usersFilePath, changeUser);
+				res.redirect('/');
+			}else 
+				//res.send(errors.mapped());
+				res.render('register', {errors: errors.mapped(), old: req.body});
     },
 	profile: (req,res) => {
 		console.log('LLEGATE')
