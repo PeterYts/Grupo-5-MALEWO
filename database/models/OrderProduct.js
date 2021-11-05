dfb61803954b6d589a99a863d87a738835e3f638
joaquinSpuches <spuchesjoaquin@gmail.com>
2021-11-04 22:11:05 -0300

    model orderProducts

---
+++ database/models/OrderProduct.js
@@ -0,0 +1,19 @@
+const { sequelize } = require(".");
+
+module.exports = (sequelize, dataTypes)=>{
+    const OrderProduct = sequelize.define('OrderProducts', {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        productId: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        orderId: {
+            type: dataTypes.INTEGER,
+            allowNull:false
+        }
+    })
+}
