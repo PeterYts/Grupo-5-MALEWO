a963e9bad596e4865f1020e663ccdf51474840d1
PeterYts <pedroytsma@gmail.com>
2021-12-09 22:27:42 -0800

    agregue carpeta de dashboard

---
+++ react-dashboard/src/components/categories.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function categories() {
+    return (
+        <h1>HOLA</h1>
+    );
+}
+
+export default categories
---
+++ react-dashboard/src/components/users.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function users() {
+    return (
+        <h1>HOLA</h1>
+    );
+}
+
+export default users
