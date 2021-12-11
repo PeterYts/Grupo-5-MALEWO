561ca2c689785533fba21c170fe9e8e80c237256
joaquinSpuches <spuchesjoaquin@gmail.com>
2021-12-11 16:39:43 -0300

    commit

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,14 @@
+var session = require('express-session')
+const fs = require('fs');
+const path = require('path');
+const {validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+const db = require('../database/models');
+
 const indexController = {
     home: (req, res) => {
-      res.render("index");
+    console.log(req.session)
+      res.render("index", {user : req.session.userLogged});
     },
     productos : (req,res) => {
         res.render ('products')
