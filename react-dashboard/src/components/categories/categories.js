42bf5ca15f744732aae6583880f045416f74b215
PeterYts <pedroytsma@gmail.com>
2021-12-13 01:53:17 -0800

    terminado dashboard en react

--- react-dashboard/src/components/categories/categories.js
+++ react-dashboard/src/components/categories/categories.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import './styles-categories.css';
 
 function Categories() {
 
-    let [categories, setCategories] = useState([]);
+    let [data, setCategories] = useState([]);
+    let extData = useRef();
+    let intData = useRef();
+    let accData = useRef();
 
     useEffect(() => {
         fetch('http://localhost:3050/products/list')
@@ -13,18 +16,29 @@ function Categories() {
             })
             .catch(err => console.log(err));
     }, []);
+
+    const showDataExt = () => {
+        extData.current.classList.toggle('new-p');
+    }
+    const showDataInt = () => {
+        intData.current.classList.toggle('new-p');
+    }
+    const showDataAcc = () => {
+        accData.current.classList.toggle('new-p');
+    }
+
     return (
         <div className="categories">
         <section>
             <h2 className="title">Categorías</h2>
-            <h4 className="totalTitle">Total: {categories.countCategories}</h4>
+            <h4 className="totalTitle">Total: {data.countCategories}</h4>
             <ul className="list">
-                <li>EXTERIOR<i class="fas fa-chevron-down arrow"></i></li>
-                <p className="">cant. productos:{categories.countByCategory.exterior}</p>
-                <li>INTERIOR <i class="fas fa-chevron-down arrow"></i></li>
-                <p className="">cant. productos: y</p>
-                <li>ACCESORIOS <i class="fas fa-chevron-down arrow"></i></li>
-                <p className="">cant. productos: z</p>
+                <li>EXTERIOR<i class="fas fa-chevron-down arrow" onClick={showDataExt}></i></li>
+                <p ref={extData}>• cant. productos: {data.countByCategory?.EXTERIOR}</p>
+                <li>INTERIOR <i class="fas fa-chevron-down arrow" onClick={showDataInt}></i></li>
+                <p ref={intData}>• cant. productos: {data.countByCategory?.INTERIOR}</p>
+                <li>ACCESORIOS <i class="fas fa-chevron-down arrow" onClick={showDataAcc}></i></li>
+                <p ref={accData}>• cant. productos: {data.countByCategory?.ACCESORIOS}</p>
             </ul>
         </section>
         </div>
